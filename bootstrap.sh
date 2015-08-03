cc8c3974a5139d10c008fd0d671cb3d8fd7b05c6
Icaro Camelo <icaro.camelo@hp.com>
2015-08-03 16:24:50 -0400

    new version

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,12 @@
 
 apt-get update
 apt-get install -y maven git openjdk-7-jre openjdk-7-jdk unzip git
-git clone https://github.com/opendaylight/controller
-git checkout stable/helium
-cd controller
-mvn clean install
 export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64
 ##Add to ~/.bashrc for persistence through a reboot##
 echo "export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64" >> ~/.bashrc
 
 # To load mininet
-apt-get install -y mininet/precise-backports
+git clone git://github.com/mininet/mininet
+git checkout -b 2.1.0p1 2.1.0p1
+sudo ./mininet/util/install.sh -n
+sudo apt-get install -y mininet/precise-backports
