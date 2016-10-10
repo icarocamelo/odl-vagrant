03536fb0da3919489181a215ebe8f1036be976ac
Icaro Camelo <icarorvc@gmail.com>
2016-10-10 17:23:19 -0400

    Improved logs

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,35 +21,40 @@ tar -zxf apache-maven-3.3.9-bin.tar.gz
 sudo cp -R apache-maven-3.3.9 /usr/local
 sudo rm /usr/bin/mvn
 sudo ln -s /usr/local/apache-maven-3.3.9/bin/mvn /usr/bin/mvn
-
-echo "export M2_HOME=/usr/local/apache-maven-3.3.3" >> ~/.profile
+echo 'Unseting M2_HOME..."
+unset M2_HOME
+echo 'Exporting new M2_HOME..."
+echo "export M2_HOME=/usr/local/apache-maven-3.3.9" >> ~/.profile
 source ~/.profile
 
 echo "Maven is on version `mvn --version`"
 
 mkdir -p ~/opendaylight/yanglab
 cd ~/opendaylight
+echo 'Cloning OpenDaylight repositories..."
 git clone https://github.com/opendaylight/odlparent.git
 git clone https://github.com/opendaylight/yangtools.git
 git clone https://github.com/opendaylight/controller.git
 
 cd ~/opendaylight/odlparent
+echo 'Compiling odlparent..."
 mvn clean install -DskipTests
 
 cd ~/opendaylight/yangtools
+echo 'Compiling yangtools..."
 mvn clean install -DskipTests
 
 cd ~/opendaylight/controller
+echo 'Compiling controller..."
 mvn clean install -DskipTests
 
 cd ~/opendaylight/yanglab
+echo "**** VM setup successfully! *** "
 echo "YANG lab base path: ~/opendaylight/yanglab"
-echo "VM setup successfully!"
 echo "To create your YANG lab you should execute the following command and set requested properties..."
 
 echo "mvn archetype:generate \
 -DarchetypeGroupId=org.opendaylight.controller \
 -DarchetypeArtifactId=opendaylight-startup-archetype \
 -DarchetypeVersion=1.2.0-Boron \
--DarchetypeRepository=http://nexus.opendaylight.org/content/repositories/opendaylight.release/
--DarchetypeCatalog=http://nexus.opendaylight.org/content/repositories/opendaylight.release/archetype-catalog.xml"
+-DarchetypeRepository=http://nexus.opendaylight.org/content/repositories/opendaylight.release/ -DarchetypeCatalog=http://nexus.opendaylight.org/content/repositories/opendaylight.release/archetype-catalog.xml"
